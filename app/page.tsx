12b253d111092def2844a477ef1d4203101d8107
Anssi Peltola <anssipeltola@hotmail.com>
2026-01-28 20:47:23 +0200

    GET total walking stats
    Closes #12
    
    Fetch all total stats from database, sessions, duration, distance, steps and calories. Made new type WalkStats to types and tested that its working.

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,23 @@
-import { getAllWalkSessions } from "@/db/queries";
+import { getAllWalkSessions, getWalkStats } from "@/db/queries";
 import AddWalkForm from "@/components/walk/AddWalkForm";
 
 export default async function Home() {
   const allSessions = await getAllWalkSessions ()
   console.log('All walk sessions:', allSessions)
 
+  const totalStats = await getWalkStats();
+  console.log('Total sessions: ' + totalStats.totalSessions);
+  console.log('Total durationSec: ' + totalStats.totalDurationSec);
+  console.log('Total distanceKm: ' + totalStats.totalDistanceKm);
+  console.log('Total steps: ' + totalStats.totalSteps);
+  console.log('Total calories: ' + totalStats.totalCalories);
+
   return (
     <main className="min-h-screen flex flex-col items-center justify-start p-8 bg-gray-50 dark:bg-gray-900">
       <h1 className="text-3xl font-bold text-blue-600 mb-6">All Walk Sessions (Test)</h1>
 
       <pre className="w-full max-w-4xl p-4 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 rounded-lg shadow overflow-x-auto">
-        {JSON.stringify(allSessions, null, 2)}
+        {JSON.stringify(totalStats, null, 2)}
       </pre>
 
       <AddWalkForm />
--- db/queries.ts
+++ db/queries.ts
@@ -1,6 +1,7 @@
+import { sql } from 'drizzle-orm'
 import { db } from './client'
 import { walkSessions } from './schema'
-import { NewWalkSession } from '@/types/walk'
+import { NewWalkSession, WalkStats } from '@/types/walk'
 
 // Fetch all walking sessions from database
 export async function getAllWalkSessions() {
@@ -51,6 +52,25 @@ export async function createWalkSession(data: NewWalkSession) {
     }) // consider .returning here later
   } catch (err) {
     console.error('Failed to insert walk session' , err)
+    throw err
+  }
+}
+
+export async function getWalkStats(): Promise<WalkStats> {
+  try {
+    const result = await db
+      .select({
+        totalSessions: sql<number>`count(*)`,
+        totalDurationSec: sql<number>`coalesce(sum(${walkSessions.durationSec}), 0)`,
+        totalDistanceKm: sql<number>`coalesce(sum(${walkSessions.distanceKm}), 0)`,
+        totalSteps: sql<number>`coalesce(sum(${walkSessions.steps}), 0)`,
+        totalCalories: sql<number>`coalesce(sum(${walkSessions.calories}), 0)`,
+      })
+      .from(walkSessions)
+
+    return result[0]
+  } catch (err) {
+    console.error('Failed to fetch walk stats:', err)
     throw err
   }
 }

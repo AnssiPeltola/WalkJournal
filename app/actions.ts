601f48f93a806c056b4229699547f12c42205c16
Anssi Peltola <anssipeltola@hotmail.com>
2026-01-29 21:21:10 +0200

    Refresh page after succesfully added new walk session
    Closes #27
    
    Refreshes now page after succesfully added new walking session into database.

--- app/actions.ts
+++ app/actions.ts
@@ -2,7 +2,9 @@
 
 import { createWalkSession } from '@/db/queries'
 import { NewWalkSession } from '@/types/walk'
+import { refresh } from 'next/cache'
 
 export async function addWalkSessionAction(data: NewWalkSession) {
   await createWalkSession(data)
+  refresh()
 }

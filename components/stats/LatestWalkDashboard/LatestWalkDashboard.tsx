cdca2940921f6eba941346e9a08b7111c16df0a5
Anssi Peltola <anssipeltola@hotmail.com>
2026-01-31 16:18:10 +0200

    Add time to LatestWalkDashboard
    Closes #29
    
    Created new file in utils folder formatDateTime.ts to format time from UTC to Helsinki/Finland time. Example 09:07 UTC is now shown as 11:07 since we have winter time at Finland now.
    
    Modified LatestWalkDashboard to use StatCard with label Created At with the value from formated time.

--- components/stats/LatestWalkDashboard/LatestWalkDashboard.tsx
+++ components/stats/LatestWalkDashboard/LatestWalkDashboard.tsx
@@ -1,6 +1,7 @@
 import { WalkSession } from '@/types/walk';
 import { formatDuration } from '@/utils/formatDuration';
 import StatCard from '@/components/stats/StatCard';
+import { formatDateTimeUTCToHelsinki } from '@/utils/formatDateTime';
 
 type Props = {
   session: WalkSession | null;
@@ -11,15 +12,9 @@ export default function LatestWalkDashboard({ session }: Props) {
   // If no sessions in database return this
   if (!session) return <p>No walk sessions yet.</p>;
 
-  const formattedDate = new Date(session.date).toLocaleDateString('fi-FI', {
-    day: '2-digit',
-    month: '2-digit',
-    year: 'numeric',
-  });
-
   return (
     <section className="stats-grid max-w-md mx-auto">
-      <StatCard label="Date" value={formattedDate} />
+      <StatCard label="Created At" value={formatDateTimeUTCToHelsinki(session.createdAt)} />
       <StatCard label="Duration" value={formatDuration(session.durationSec)} />
       <StatCard label="Distance" value={`${session.distanceKm} km`} />
       <StatCard label="Steps" value={session.steps} />
